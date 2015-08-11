da3c407122ec5512b98d2a1184d2201c11f50496
Stephan Wentz <swentz@brainbits.net>
2015-08-11 10:15:48 +0200

    import

---
+++ app/AppKernel.php
@@ -0,0 +1,72 @@
+<?php
+
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Config\Loader\LoaderInterface;
+
+class AppKernel extends Kernel
+{
+    public function registerBundles()
+    {
+        $bundles = array(
+            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
+            new Symfony\Bundle\TwigBundle\TwigBundle(),
+            new Symfony\Bundle\MonologBundle\MonologBundle(),
+            new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
+            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new AppBundle\AppBundle(),
+
+            new Igorw\FileServeBundle\IgorwFileServeBundle(),
+            new Nelmio\ApiDocBundle\NelmioApiDocBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
+            new Puli\SymfonyBundle\PuliBundle(),
+            new Symfony\Cmf\Bundle\RoutingBundle\CmfRoutingBundle(),
+
+            // phlexible.core
+            new Phlexible\Bundle\GuiBundle\PhlexibleGuiBundle(),
+            new Phlexible\Bundle\MessageBundle\PhlexibleMessageBundle(),
+            new Phlexible\Bundle\QueueBundle\PhlexibleQueueBundle(),
+            new Phlexible\Bundle\UserBundle\PhlexibleUserBundle(),
+            new Phlexible\Bundle\ProblemBundle\PhlexibleProblemBundle(),
+            new Phlexible\Bundle\DashboardBundle\PhlexibleDashboardBundle(),
+            new Phlexible\Bundle\SearchBundle\PhlexibleSearchBundle(),
+            new Phlexible\Bundle\AccessControlBundle\PhlexibleAccessControlBundle(),
+            new Phlexible\Bundle\DataSourceBundle\PhlexibleDataSourceBundle(),
+
+            // phlexible.media
+            new Phlexible\Bundle\MediaCacheBundle\PhlexibleMediaCacheBundle(),
+            new Phlexible\Bundle\MediaExtractorBundle\PhlexibleMediaExtractorBundle(),
+            new Phlexible\Bundle\MediaManagerBundle\PhlexibleMediaManagerBundle(),
+            new Phlexible\Bundle\MediaTemplateBundle\PhlexibleMediaTemplateBundle(),
+            new Phlexible\Bundle\MediaToolBundle\PhlexibleMediaToolBundle(),
+            new Phlexible\Bundle\MediaTypeBundle\PhlexibleMediaTypeBundle(),
+            new Phlexible\Bundle\MetaSetBundle\PhlexibleMetaSetBundle(),
+
+            // phlexible.cms
+            new Phlexible\Bundle\CmsBundle\PhlexibleCmsBundle(),
+            new Phlexible\Bundle\SiterootBundle\PhlexibleSiterootBundle(),
+            new Phlexible\Bundle\ElementtypeBundle\PhlexibleElementtypeBundle(),
+            new Phlexible\Bundle\ElementBundle\PhlexibleElementBundle(),
+            new Phlexible\Bundle\TeaserBundle\PhlexibleTeaserBundle(),
+            new Phlexible\Bundle\TreeBundle\PhlexibleTreeBundle(),
+            new Phlexible\Bundle\FrontendBundle\PhlexibleFrontendBundle(),
+            new Phlexible\Bundle\FrontendMediaBundle\PhlexibleFrontendMediaBundle(),
+            new Phlexible\Bundle\ElementRendererBundle\PhlexibleElementRendererBundle(),
+        );
+
+        if (in_array($this->getEnvironment(), array('dev', 'test'))) {
+            $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
+            $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
+            $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
+            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+        }
+
+        return $bundles;
+    }
+
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        $loader->load($this->getRootDir().'/config/config_'.$this->getEnvironment().'.yml');
+    }
+}

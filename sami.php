fe160661f208c8bdbdbb92de1a78124f83a3ed74
Stephan Wentz <swentz@brainbits.net>
2015-09-14 15:19:52 +0200

    add phpmetrics and sami config, fix phpunit config

---
+++ sami.php
@@ -0,0 +1,21 @@
+<?php
+
+use Sami\Sami;
+use Symfony\Component\Finder\Finder;
+
+$iterator = Finder::create()
+    ->files()
+    ->name('*.php')
+    ->exclude('build')
+    ->exclude('Resources')
+    ->exclude('Tests')
+    ->exclude('vendor')
+    ->in(__DIR__.'/src/Tipfinder')
+;
+
+return new Sami($iterator, array(
+    'title'                => 'Project API',
+    'build_dir'            => __DIR__.'/build/apidocs',
+    'cache_dir'            => __DIR__.'/build/cache',
+    'default_opened_level' => 2,
+));
